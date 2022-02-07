f98303c017d84ab66754e075c13a17ecba85ea72
Henrique <henriquems@gmail.com>
2022-02-07 19:20:17 -0300

    Home, navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {
 import NavBar from "components/Navbar";
 import Home from "pages/Home";
 import Dashboard from "pages/Dashboard";
+import Footer from "components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
+        <Footer />
     </BrowserRouter>
   );
 }
